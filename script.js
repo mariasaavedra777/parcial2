2ab74210dff0ba35266ca99e802ab020aa555eb3
mariasaavedra777 <est.mariaf.saavedra@unimilitar.edu.co>
2026-04-24 09:59:58 -0500

    Estructura base HTML5 y configuración del Contexto 2D del Canvas.
    Setup: Estructura HTML5 y definición de contexto 2D.

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * Universidad - Facultad de Ingeniería
+ * Asignatura: Introducción a la Computación Gráfica
+ * * Estudiante: Maria Fernanda Saavedra Amortegui
+ * * Tarea: Implementar los algoritmos de rasterización manual.
+ */
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
